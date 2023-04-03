088d45d8b4d937163fd935ceb088f8616e333cd0
resh1995maas <129730758+resh1995maas@users.noreply.github.com>
2023-04-03 14:09:35 +0530

    Initial commit

---
+++ basics1.js
@@ -0,0 +1,35 @@
+
+// var a=['meera','ammu',10];
+// a.push('orange')
+// console.log(a);
+// a.pop(a);
+// console.log(a);
+
+// let person={
+//    name:'Reshma',
+//    age:27,
+//    location:'kollam'
+// }
+// // console.log(person.location);
+// function mul(a,b) {
+//    var mul=a*b;
+//    console.log(mul);
+// }
+// mul(90,9);
+
+
+   
+// function div(a,b) {
+//    var div=a/b;
+//    console.log(div);
+
+   
+// }
+// div(20,10);
+// function add(a,b) {
+//    var add=a+b;
+//    return add;
+   
+// }
+// var sum=add(20,10);
+// console.log(sum);
---
+++ script.js
@@ -0,0 +1,15 @@
+// console.log("welcome");
+// for(let i=0;i<=10;i++){
+// console.log(i);
+// }
+    
+// for(let i=10;i>=0;i--){
+//     console.log(i);
+//     }
+// 
+let i=10;
+while(i<=10)
+{
+    console.log(i);
+    i++;
+}
